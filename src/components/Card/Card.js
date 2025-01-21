cf0b32ac75242bbf95e83715bd2b099d9d9bca3f
Pankaj Hadole <pankajhadole24@gmail.com>
2025-01-21 22:51:23 +0530

    Update Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -27,7 +27,7 @@ const Card = ({ cardItem }) => {
       {/* Card Image */}
       {cardItem.image && (
         <img
-          src={`http://admin.truskill.in${cardItem.image.url}`}
+          src={`https://strapi.truskill.in${cardItem.image.url}`}
           alt={cardItem.heading}
           className="w-full h-40 object-cover rounded-t-2xl p-2"
         />
