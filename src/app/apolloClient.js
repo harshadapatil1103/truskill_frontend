67753cae4767d01ce8211fe34f2c237885703027
Pankaj Hadole <pankajhadole24@gmail.com>
2025-01-21 21:45:21 +0530

    Update apolloClient.js

--- src/app/apolloClient.js
+++ src/app/apolloClient.js
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://admin.truskill.in/graphql', // Strapi GraphQL endpoint
+  uri: 'http://strapi.truskill.in/graphql', // Strapi GraphQL endpoint
   cache: new InMemoryCache(),
 });
 
