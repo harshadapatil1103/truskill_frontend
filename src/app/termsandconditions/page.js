0ab7c37f06f8d8d1a095a159aa96e8e5a9c5613b
Krishna Patil <krishnapatil6446@gmail.com>
2025-02-16 22:11:33 +0530

    added privacy policy and terms and conditions

--- src/app/termsandconditions/page.js
+++ src/app/termsandconditions/page.js
@@ -4,7 +4,32 @@ import React from 'react'
 function page() {
   return (
     <div >
-    <h1 className="font-semibold text-5xl my-56 text-center ">Coming Soon..</h1>
+<h1 className="font-semibold text-2xl mt-24 mb-4 text-center ml-12">
+  Terms and Conditions
+</h1>
+<hr className="border-t-2 border-black ml-6 w-[95%] mb-10" />
+
+<p className="text-lg text-black ml-12 mb-6">
+
+    • This document is an electronic record in terms of the Information Technology Act, 2000 and rules thereunder as applicable. <br /> 
+    • By using the Platform, you agree to provide true, accurate, and complete information to us during and after registration, and you shall be responsible for all acts done through the use of your registered account. <br /> 
+    • Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness, or suitability of the information and materials offered on this website or through the Services. <br /> 
+    • Your use of our Services and the Platform is solely and entirely at your own risk and discretion, for which we shall not be liable to you in any manner. <br /> 
+    • The contents of the Platform and the Services are proprietary to us and are licensed to us. You will not have any authority to claim any intellectual property rights, title, or interest in its contents. <br /> 
+    • You acknowledge that unauthorized use of the Platform and/or the Services may lead to action against you as per these Terms of Use and/or applicable laws. <br /> 
+    • You agree to pay us the charges associated with availing the Services. <br /> 
+    • You agree not to use the Platform and/or Services for any purpose that is unlawful, illegal, or forbidden by these Terms, or Indian or local laws that might apply to you. <br /> 
+    • You agree and acknowledge that the website and the Services may contain links to other third-party websites. On accessing these links, you will be governed by the terms of use, privacy policy, and such other policies of such third-party websites. <br /> 
+    • You understand that upon initiating a transaction for availing the Services, you are entering into a legally binding and enforceable contract with the Platform Owner for the Services. <br /> 
+    • You shall indemnify and hold harmless the Platform Owner, its affiliates, group companies, and their respective officers, directors, agents, and employees from any claim or demand, or actions including reasonable attorney's fees, made by any third party due to your breach of these Terms of Use, Privacy Policy, or other policies. <br /> 
+    • Notwithstanding anything contained in these Terms of Use, the parties shall not be liable for any failure to perform an obligation under these Terms if performance is prevented or delayed by a force majeure event. <br /> 
+    • These Terms and any dispute or claim relating to them, or their enforceability, shall be governed by and construed in accordance with the laws of India. <br /> 
+    • All disputes arising out of or in connection with these Terms shall be subject to the exclusive jurisdiction of the courts in Mumbai, Maharashtra. <br /> 
+    • All concerns or communications relating to these Terms must be communicated to us using the contact information provided on this website. <br /> 
+
+
+</p>
+
     </div>
   )
 }
